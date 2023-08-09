fe8b93338b216d888ac92162b165513cfa546d73
darkninja <kevinshah9920@gmail.com>
2023-08-09 22:47:44 +0530

    Yourtube

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("searchButton").addEventListener("click", function() {
+    const searchTerm = document.getElementById("searchBar").value;
+    // Perform search functionality here
+    // You can fetch and display videos dynamically
+});
